d01deea8f4f887e92f14f033c8d633abd176aecb
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-10-08 10:46:50 -0300

    Python 10 Pass

--- cupom.py
+++ cupom.py
@@ -1,4 +1,7 @@
 # coding: utf-8
+def isEmpty(value:str) -> bool:
+        return (value == None) or (len(value) == value.count(" "))
+
 
 class Loja:
   
@@ -20,4 +23,53 @@ class Loja:
 
   def dados_loja(self):
     # Implemente aqui
-    return "" 
+    if (isEmpty(self.nome_loja)):
+      raise Exception("O campo nome da loja é obrigatório")
+    if (isEmpty(self.logradouro)):
+      raise Exception("O campo logradouro do endereço é obrigatório")
+
+    numero = int()
+    try:
+      numero = int(self.numero)
+    except Exception:
+      numero = 0
+    if(numero<=0):
+      numero = "s/n"
+
+    if(isEmpty(self.cnpj)):
+        raise Exception("O campo CNPJ da loja é obrigatório")
+    if (isEmpty(self.municipio)):
+        raise Exception("O campo município do endereço é obrigatório")
+    if (isEmpty(self.estado)):
+        raise Exception("O campo estado do endereço é obrigatório")
+    if (isEmpty(self.inscricao_estadual)):
+        raise Exception("O campo inscrição estadual da loja é obrigatório")
+
+    part2 = f"{self.logradouro}, {numero}"
+    if not isEmpty (self.complemento):
+      part2 += f" {self.complemento}"
+    part3 = str()
+    if not isEmpty (self.bairro):
+        part3 += f"{self.bairro} - "
+    part3 += f"{self.municipio} - {self.estado}"
+    part4 = str()
+    if not isEmpty (self.cep):
+        part4 = f"CEP:{self.cep}"
+    if not isEmpty(self.telefone):
+        if not isEmpty(part4):
+            part4 += " "
+        part4 += f"Tel {self.telefone}"     
+    if not isEmpty(part4):
+        part4 += "\n"
+    part5 = str()
+    if not isEmpty(self.observacao):
+        part5 += f"{self.observacao}"
+    
+    output = f"{self.nome_loja}\n"
+    output += f"{part2}\n"
+    output += f"{part3}\n"
+    output += f"{part4}"
+    output += f"{part5}\n"
+    output += f"CNPJ: {self.cnpj}\n"
+    output += f"IE: {self.inscricao_estadual}"
+    return output
